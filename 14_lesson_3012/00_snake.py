05b0753db55e4fe2de1b334ef677a022090f3fc3
demidraksin <greatraksin@icloud.com>
2021-12-30 15:00:07 +0300

    Adding animate snow template 🧠

---
+++ 14_lesson_3012/00_snake.py
@@ -0,0 +1,23 @@
+import pygame as pg
+
+
+def setup() -> object:
+    pg.init()
+    screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)  # окошко открывается на весь экран
+    clock = pg.time.Clock()
+    return screen, clock
+
+
+def main(surface: object, updater: object) -> None:
+    done = False
+    while not done:
+        surface.fill(pg.Color('cadetblue1'))
+        for event in pg.event.get():
+            if (event.type == pg.QUIT) or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                done = True
+        pg.display.flip()  # обновление экрана
+        updater.tick(60)  # FPS игры
+
+
+screen, clock = setup()
+main(screen, clock)
