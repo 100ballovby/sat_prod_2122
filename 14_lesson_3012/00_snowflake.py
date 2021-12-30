923b545e6f899542235cd0f6f87d641ee73635d1
demidraksin <greatraksin@icloud.com>
2021-12-30 15:20:50 +0300

    Adding snowflakes generator ❄️

--- 14_lesson_3012/00_snowflake.py
+++ 14_lesson_3012/00_snowflake.py
@@ -1,6 +1,7 @@
 import sys
 import pygame as pg
 import random as r
+from pygame.draw import circle
 
 
 def setup() -> object:
@@ -13,12 +14,19 @@ def setup() -> object:
 def main(surface: object, updater: object) -> None:
     done = False
     snowflakes = []
+    for i in range(150):  # рисую много снежинок
+        x = r.randint(0, surface.get_width())
+        y = r.randint(0, surface.get_height())
+        snowflakes.append((x, y))
+
     while not done:
         surface.fill(pg.Color('cadetblue1'))
         for event in pg.event.get():
             if (event.type == pg.QUIT) or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 done = True
                 sys.exit()  # правильное ЗАКРЫТИЕ окна программы
+        for sf in snowflakes:
+            circle(surface, pg.Color('snow1'), (sf[0], sf[1]), r.randint(2, 6))  # размер каждой снежинки меняется
         pg.display.flip()  # обновление экрана
         updater.tick(60)  # FPS игры
 
